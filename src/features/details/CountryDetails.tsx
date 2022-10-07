83bf49da247c1457e66e8f164634c503ae185301
Соколов Глеб <g.sokolov@21vek.local>
2022-10-07 15:17:02 +0300

    type details

--- src/features/details/CountryDetails.tsx
+++ src/features/details/CountryDetails.tsx
@@ -1,8 +1,12 @@
+import { NavigateFunction } from 'react-router-dom';
 import { Info } from './Info';
 import { useDetails } from './use-details';
 
-
-const CountryDetails = ({name = '', navigate}) => {
+interface CountryDetailsProps {
+  name?: string,
+  navigate: NavigateFunction
+}
+const CountryDetails = ({name = '', navigate}: CountryDetailsProps) => {
   const {status, error, currentCountry} = useDetails(name);
 
   return (
